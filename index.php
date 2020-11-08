4003d04fc611ae8b5a62c74d0e2fc1a370fd3131
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-08 12:01:39 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hat Shop</title>
+</head>
+<body>
+    <h2>Database info</h2>
+    <?php
+
+        $servername = "0.0.0.0";
+        $username = "priori";
+        $password = "password";
+        $dbname = "HatShop";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "Select * from products";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                echo "<br>id: " . $row["id"] . " <br> Name: " . $row["name"] . "<br>Description: " . $row["description"] . "<br>type: " . $row["type"] . "<br>price: " . $row["price"];
+            }
+        }else{
+            echo "0 results";
+        }
+        $conn->close();
+
+    ?>
+
+
+</body>
+</html>
