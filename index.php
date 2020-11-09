8c4edd8269dca9d7f9b5934ea17cc0b57e2a1ca6
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-09 19:29:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
-                echo $row["name"] . ": " . $row["price"] . "€ " . "<a href='#'' onclick=\"requestDetailsPage(".$row['id'].");\" class='btn btn-warning btn-sm'>View</a>" . "<br><br>" ;
+                echo $row["name"] . ": " . $row["price"] . "€ " . "<a href='product.php'' onclick=\"requestDetailsPage(".$row['id'].");\" class='btn btn-warning btn-sm'>View</a>" . "<br><br>" ;
                 
             }
         }else{
