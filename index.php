af561e446914f4091944b8583e50456f61d6464a
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-09 18:59:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
         if($result->num_rows > 0){
             while($row = $result->fetch_assoc()){
-                echo $row["name"] . ": " . $row["price"] . "<br><br>";
+                echo $row["name"] . ": " . $row["price"] . "€" . "<br><br>";
             }
         }else{
             echo "0 results";
