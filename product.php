2ab6711176c282f69d8a03d45ad86e3f978e9927
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-10 10:25:42 +0100

    Update product.php

--- product.php
+++ product.php
@@ -8,7 +8,7 @@
 <body>
 <h2>Juan's Hats</h2>
 <?php 
-    //if (isset($_GET['id'])) {
+    if (isset($_GET['id'])) {
         // Connect to the MySQL database  
         $servername = "0.0.0.0";
         $username = "priori";
@@ -22,17 +22,17 @@
             die("Connection failed: " . $conn->connect_error);
         }
 
-        if(isset($_GET['id'])){
+        /*if(isset($_GET['id'])){
             $id = htmlentities($_GET['id']);
 
             $query = "SELECT * FROM table WHERE id = " . $id;
         } else{
             $query = "SELECT * FROM table ORDER BY id";
-        } 
+        } */
 
-        //$sql = "SELECT * FROM products WHERE id='$id' LIMIT 1";
-        //$result = $conn->query($sql); 
-        if ($result = $mysqli->query($query)) {
+        $sql = "SELECT * FROM products WHERE id='$id' LIMIT 1";
+        $result = $mysqli->query($sql); 
+        //if ($result = $mysqli->query($query)) {
             if($result->num_rows < 0){
                 while($row = $result->fetch_object()){ 
                     $product_name = $row["name"];
@@ -46,9 +46,12 @@
                 echo "That item does not exist.";
                 
             }
-        } else{
-            echo "Theres a problem with the query.";
-        }
+        //} else{
+         //   echo "Theres a problem with the query.";
+        //}
+    }else{
+        echo "Theres a problem with the query";
+    }
     $mysqli->close();
 ?>
 </body>
