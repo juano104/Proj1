66d4b9501ebebc41ec90e3f8570ce3b45f513fa7
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-10 10:39:28 +0100

    Update product.php

--- product.php
+++ product.php
@@ -21,7 +21,7 @@
         if ($mysqli->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        $id = $_GET['id'];
+        $id = preg_replace('#[^0-9]#i', '', $_GET['id']);
 
         /*if(isset($_GET['id'])){
             $id = htmlentities($_GET['id']);
