b8f0c22b28f8e870a30eac393283795ac78a8f59
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-11 13:54:49 +0100

    Update HatShop.sql

--- config/HatShop.sql
+++ config/HatShop.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS HatShop;
 USE HatShop;
 
 CREATE TABLE products(
-	id int not null,
+	id decimal not null,
     name varchar(50),
     description varchar(150),
     type varchar(10), -- summer, formal, travel, everyday, luxury
@@ -14,17 +14,17 @@ CREATE TABLE products(
 );
 
 insert into products
-values (1, "Black Hat", "Comfortable hat for an everyday use.", "everyday", 16),
-	(2, "Blue Hat", "A hat for traveling.", "summer", 19),
-    (3, "Red Hat", "For a nice date out.", "formal", 17),
-    (4, "Green Hat", "Comfortable hat for an everyday use.", "everyday", 16),
-	(5, "Yellow Hat", "A hat for traveling.", "summer", 19),
-    (6, "Orange Hat", "Comfortable hat for an everyday use.", "everyday", 16),
-	(7, "Dark Blue Hat", "A hat for traveling.", "summer", 19),
-    (8, "Purple Hat", "Comfortable hat for an everyday use.", "everyday", 16),
-	(9, "Light Orange Hat", "A hat for traveling.", "summer", 19),
-    (10, "Light Green Hat", "Comfortable hat for an everyday use.", "everyday", 16),
-    (11, "White Hat", "A hat for traveling.", "summer", 19),
-    (12, "Pink Hat", "Comfortable hat for an everyday use.", "everyday", 16);
+values (1, "Black Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	(2, "Blue Hat", "A hat for traveling.", "summer", 16.99),
+    (3, "Red Hat", "For a nice date out.", "formal", 17.99),
+    (4, "Green Hat", "Comfortable hat for an everyday use.", "everyday", 15.99),
+	(5, "Yellow Hat", "A hat for traveling.", "summer", 17.99),
+    (6, "Orange Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	(7, "Dark Blue Hat", "A hat for traveling.", "summer", 18.99),
+    (8, "Purple Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	(9, "Light Orange Hat", "A hat for traveling.", "summer", 19.99),
+    (10, "Light Green Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+    (11, "White Hat", "A hat for traveling.", "summer", 17.99),
+    (12, "Pink Hat", "Comfortable hat for an everyday use.", "everyday", 15.99);
 
 select * from products;
