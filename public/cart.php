e90a59e15b82e75f02ca88683e2207a8691ec2e7
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-12 20:38:36 +0100

    Update cart.php

--- public/cart.php
+++ public/cart.php
@@ -32,7 +32,7 @@ switch($_GET["action"]) {
 		if(!empty($_SESSION["cart_item"])) {
 			foreach($_SESSION["cart_item"] as $k => $v) {
 					if($_GET["id"] == $k)
-						unset($_SESSION["cart_item"][$v]);				
+						unset($_SESSION["cart_item"][$k]);				
 					if(empty($_SESSION["cart_item"]))
 						unset($_SESSION["cart_item"]);
 			}
