f9ddafb0cbc9422101a36af98c2da3dc0244ccb9
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-16 20:38:55 +0100

    added formprocess

--- public/form.php
+++ public/form.php
@@ -1,29 +1,3 @@
-<?php
-include('../config/db.php');
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if (isset($_POST['save'])) {
-    $name = $_POST['name'];
-    $description = $_POST['description'];
-    $type = $_POST['type'];
-    $price = $_POST['price'];
-    $sql = "INSERT INTO products (name,description,type,price)
-	VALUES ('$name','$description','$type','$price')";
-    if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully !<br>";
-        echo "Would you like to enter another one? <a href='form.php'>YES</a>  <a href='index.php'>NO</a>";
-    } else {
-        echo "Error: " . $sql . " " . mysqli_error($conn);
-    }
-    mysqli_close($conn);
-}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,7 +11,7 @@ if (isset($_POST['save'])) {
 
 <body>
     <h2>Enter Product</h2>
-    <form method="post" action="form.php">
+    <form method="post" action="formprocess.php">
         <div class="container">
             Product Name:<br>
             <input type="text" name="name">
