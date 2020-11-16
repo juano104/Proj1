316532690198c3147aba630ec4e3bc137a434cc4
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-16 19:44:38 +0100

    change sql to autoincrement and add form

--- config/HatShop.sql
+++ config/HatShop.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS HatShop;
 USE HatShop;
 
 CREATE TABLE products(
-	id decimal not null,
+	id int not null auto_increment,
     name varchar(50),
     description varchar(150),
     type varchar(10), -- summer, formal, travel, everyday, luxury
@@ -13,18 +13,18 @@ CREATE TABLE products(
     
 );
 
-insert into products
-values (1, "Black Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
-	(2, "Blue Hat", "A hat for traveling.", "summer", 16.99),
-    (3, "Red Hat", "For a nice date out.", "formal", 17.99),
-    (4, "Green Hat", "Comfortable hat for an everyday use.", "everyday", 15.99),
-	(5, "Yellow Hat", "A hat for traveling.", "summer", 17.99),
-    (6, "Orange Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
-	(7, "Dark Blue Hat", "A hat for traveling.", "summer", 18.99),
-    (8, "Purple Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
-	(9, "Light Orange Hat", "A hat for traveling.", "summer", 19.99),
-    (10, "Light Green Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
-    (11, "White Hat", "A hat for traveling.", "summer", 17.99),
-    (12, "Pink Hat", "Comfortable hat for an everyday use.", "everyday", 15.99);
+insert into products (name, description, type, price)
+values ("Black Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	("Blue Hat", "A hat for traveling.", "summer", 16.99),
+    ("Red Hat", "For a nice date out.", "formal", 17.99),
+    ("Green Hat", "Comfortable hat for an everyday use.", "everyday", 15.99),
+	("Yellow Hat", "A hat for traveling.", "summer", 17.99),
+    ("Orange Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	("Dark Blue Hat", "A hat for traveling.", "summer", 18.99),
+    ("Purple Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+	("Light Orange Hat", "A hat for traveling.", "summer", 19.99),
+    ("Light Green Hat", "Comfortable hat for an everyday use.", "everyday", 16.99),
+    ("White Hat", "A hat for traveling.", "summer", 17.99),
+    ("Pink Hat", "Comfortable hat for an everyday use.", "everyday", 15.99);
 
 select * from products;
