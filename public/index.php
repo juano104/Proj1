ae2e31d65858818b5e1b12c8d1f28a38cedbd08d
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-11-26 20:28:14 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -9,7 +9,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$_GET['lang'] = "en";
 // Set Language variable
 if (isset($_GET['lang']) && !empty($_GET['lang'])) {
     $_SESSION['lang'] = $_GET['lang'];
@@ -24,6 +23,7 @@ if (isset($_SESSION['lang'])) {
     include "lang_" . $_SESSION['lang'] . ".php";
 } else {
     include "lang_en.php";
+    $_GET['lang'] = "en";
 }
 //make query
 if ($_GET['lang'] == "es") {
