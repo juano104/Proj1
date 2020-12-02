171aa7567e0b890a04c0a009422f7721781c0728
Juan Priori <jpriori14500@alumnes.iesmanacor.cat>
2020-12-02 13:17:01 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,15 +10,15 @@ if ($conn->connect_error) {
 }
 
 // Set Language variable
-if (isset($_GET['lang']) /*&& !empty($_GET['lang'])*/) {
+if (isset($_GET['lang']) && !empty($_GET['lang'])) {
     $_SESSION['lang'] = $_GET['lang'];
 
     if (isset($_SESSION['lang']) && $_SESSION['lang'] != $_GET['lang']) {
         echo "<script type='text/javascript'> location.reload(); </script>";
     }
-}else{
+}/*else{
     $_SESSION['lang'] = "en";
-}
+}*/
 
 // Include Language file
 if (isset($_SESSION['lang'])) {
